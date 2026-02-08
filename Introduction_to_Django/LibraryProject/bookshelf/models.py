115a1463374e592ebce37f8d11d955bbbb310969
danzeph <danzeph00@gmail.com>
2026-02-08 23:19:00 +0000

    adding relationship models.py

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,3 +4,10 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
+
+class GeeksModel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import GeeksModel
 
-# Create your views here.
+class GeeksList(ListView):
+    model = GeeksModel
+    template_name = "geeks/geeksmodel_list.html"   # template path
+    context_object_name = "object_list"            # list name in template
