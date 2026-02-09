a34229dd8ef3dc6e5703eb3615819e864c45360e
danzeph <danzeph00@gmail.com>
2026-02-09 00:30:27 +0000

    Fix formatting of filter query for books by author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from .models import Author, Book, Library, Librarian
 
 # Retrieving all books by a particular author
 author = Author.objects.get(id=1)
-books_by_author = Book.objects.filter(author = author)
+books_by_author = Book.objects.filter(author=author)
 
 # Listing all books in a library
 library = Library.objects.get(name=library_name)
