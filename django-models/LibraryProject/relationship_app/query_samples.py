623b3299292e6c6dfc6b06378a784b332a3bd15e
danzeph <danzeph00@gmail.com>
2026-02-09 00:34:03 +0000

    Update author retrieval and library queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,12 @@
 from .models import Author, Book, Library, Librarian
 
 # Retrieving all books by a particular author
-author = Author.objects.get(id=1)
+author_name = "danamaz knows"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
 
 # Listing all books in a library
+library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
@@ -13,5 +15,3 @@ library = Library.objects.get(name=library_name)
 librarian_of_libary = library.liberian
 
 # librarian_of_libary = Librarian.objects.get(library_id=1)
-
-
