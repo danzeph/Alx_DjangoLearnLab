825ec8ee9214a0c0eae1ec439b8ab69b335d63da
danzeph <danzeph00@gmail.com>
2026-02-09 15:18:42 +0000

    updated views for book list and library.books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render
-from .models import Author, Book, Library, Librarian
-from django.views.generic import ListView, DetailView
+from .models import Book, Library
+from django.views.generic import DetailView
 
 def list_books(request):
     books = Book.objects.all()
@@ -11,10 +11,10 @@ def list_books(request):
 class BookDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     model = Library
-    # def get_object(self):
-    #     obj = Library.objects.first()
-    #     if not obj:
-    #         raise Http404("No Library object found")
-    #     return obj
+    def get_object(self):
+        obj = Library.objects.first()
+        if not obj:
+            raise Http404("No Library object found")
+        return obj
     
     
