180f99c628d78d0b769dd4abf76e78679d77b06c
danzeph <danzeph00@gmail.com>
2026-02-09 22:17:54 +0000

    authenticaton login and logout

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,7 @@
 from django.http import Http404
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import redirect, render
 from .models import Library, Book
 from django.views.generic.detail import DetailView
-from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
@@ -25,23 +22,14 @@ class LibraryDetailView(DetailView):
     
 
 
-class register(CreateView):
+def register(request):
     template_name = 'relationship_app/register.html'
-    form_class = UserCreationForm()
-    success_url = reverse_lazy('login')
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        login(self.request, self.object) 
-        return response
-
-
-
-# class UserLoginView(LoginView):
-#     template_name = 'relationship_app/login.html'
-#     next_page = reverse_lazy('book-list')
-
-
-# class UserLogoutView(LogoutView):
-#     template_name ='relationship_app/logout.html'
-    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if  form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book-list')
+        else:
+            form = UserCreationForm()
+        return render(request,template_name,{'form':form})
