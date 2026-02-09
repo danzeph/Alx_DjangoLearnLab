d2421d90ccb555a3f5fb8fa4281936843b5d727c
danzeph <danzeph00@gmail.com>
2026-02-09 21:43:31 +0000

    authenticaton login and logout

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -22,10 +24,18 @@ class LibraryDetailView(DetailView):
         return obj
     
 
+from django.contrib.auth import login
+
 class RegisterUserView(CreateView):
     template_name = 'relationship_app/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object) 
+        return response
+
 
 
 class UserLoginView(LoginView):
