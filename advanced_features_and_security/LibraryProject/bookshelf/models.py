5a3289989c74c25677cff11d8527a0ced95765c0
danzeph <danzeph00@gmail.com>
2026-02-14 04:10:19 +0000

    added Cusstome user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +12,19 @@ class GeeksModel(models.Model):
 
     def __str__(self):
         return self.title
+    
+class CustomerUserModel(AbstractUser):
+    date_of_birth = models.models.DateField()
+    profile_picture = models.ImageField()
+
+    def __str__(self):
+        return self.username
+    
+
+class UserManager(BaseUserManager):
+    def create_user(self, email,password=None):
+        pass
+
+    def create_super_user(self, email, password=None):
+        pass
+        
