3c898ad7ff48cc51a9fe16fb93367d4187771a4d
danzeph <danzeph00@gmail.com>
2026-02-22 02:11:40 +0000

    added serializers.py with updated views.pu

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from api.models import Book
-from api.serializers import BookSerializer
+from .models import Book
+from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
