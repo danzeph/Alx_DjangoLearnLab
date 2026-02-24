d6c856cde0522ef2e7ff6cab3a0abff69054a8f4
danzeph <danzeph00@gmail.com>
2026-02-24 23:19:28 +0000

    corrected ordering fields

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics #, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import OrderingFilter,SearchFilter
 from api.models import Book
-from .serializers import AuthorSerializer, BookSerializer
+from .serializers import BookSerializer
+# from rest_framework.response import Response
 
 class ListView(generics.ListAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
     filter_backends = [SearchFilter, OrderingFilter]
-    ordering_fields = ['author__name']
-    search_fields = ['title']
+    ordering_fields = ['title','publication_year']
+    search_fields = ['title', 'author']
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 
     def get_queryset(self):
         """Custom method to filter based on book title"""
-        """Custom filters based on URL"""
+        """Custom filters based on URL only"""
         queryset = Book.objects.all()
         title_filter = self.request.query_params.get('title', None)
         if title_filter is not None:
@@ -36,9 +37,20 @@ class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     permission_classes = [IsAuthenticated]
     
+    # def create(self, request, *args, **kwargs):
+    #     serializer = BookSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(
+    #         {
+    #             "message":"Book created successfully",  
+    #             "data":serializer.data
+    #         }, 
+    #         status=status.HTTP_201_CREATED
+    #         )
+
     # def perform_create(self, serializer):
     #     return super().perform_create(serializer)
-
 
 class UpdateView(generics.RetrieveUpdateAPIView):
     serializer_class = BookSerializer
