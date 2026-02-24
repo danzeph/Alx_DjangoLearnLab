6dbe5c20e5e45455f83d5d99f616bbce9d6d1ea3
danzeph <danzeph00@gmail.com>
2026-02-24 16:31:23 +0000

    Corrected urls path to match what's required

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('books/', views.ListView.as_view(),name ='books'),
     path('books/create', views.CreateView.as_view(), name='create-books'),
     path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/delete/', views.DeleteView.as_view(), name='book-delete'),
-    path('books/<int:pk>/update/', views.UpdateView.as_view(), name='book-update'),
+    path('books/delete/', views.DeleteView.as_view(), name='book-delete'),
+    path('books/update/', views.UpdateView.as_view(), name='book-update'),
     path('c', obtain_auth_token),
 ]
