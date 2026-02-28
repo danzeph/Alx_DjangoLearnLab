5c8df770136d9724ae2a1a31bfbcc22ea5d7edd1
danzeph <danzeph00@gmail.com>
2026-02-28 01:42:11 +0000

    corrected errors in user authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('register/',register , name='register'),
-    path('login/',LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/',LogoutView.as_view() , name='logout'),
+    path('login/',LoginView.as_view(template_name='log/login.html'), name='login'),
+    path('logout/',LogoutView.as_view(template_name='blog/logout.html') , name='logout'),
     path('profile/',profile_update, name='profile'),
     
 
